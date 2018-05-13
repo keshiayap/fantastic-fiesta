64488fc86a8989c9c8e07119a5962957f922c3ba
KaiTingKeshia Yap <kyap@occs.cs.oberlin.edu>
2018-05-13 16:46:59 -0400

    added node stuff

--- log.h
+++ log.h
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <memory>
 #include <QDebug>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
 #define C 1
 #define U 0
 
@@ -12,8 +17,8 @@ using namespace std;
 class Board {
  public:
   int COMPUTER; int USER; // # of pieces at start of a game
-  coordinate compList[12];
-  coordinate userList[12];
+  /* coordinate compList[12]; */
+  /* coordinate userList[12]; */
 
   char board[8][8];
   void updateBoard(coordinate current, coordinate next, char turn, bool jump);
@@ -25,16 +30,41 @@ class Board {
 
 };
 
+// Board functions
+Board::Board();
+void Board::updateBoard(coordinate curr, coordinate next, char player, bool jump);
+void Board::flip();
+void Board::toString();
+Board Board::copyBoard(Board board);
+
 // easy tuple to use to find a position on the board
 typedef struct coordinate {
   int x;
   int y;
 } coordinate;
 
+// move functions
 void check_null(void *ptr, char *msg);
-coordinate *init(int a, int b);
+coordinate init(int a, int b);
 coordinate *buildMove(int r1, int c1, int r2, int c2);
 bool canMove(int player, int r1, int c1, int r2, int c2);
 bool canJump(int player, int r1, int c1, int r2, int c2);
 coordinate **getMoves(int player);
-coordinate *getJumps(int player);
+coordinate **getJumps(int player);
+coordinate getPiece(coordinate *move);
+coordinate getDest(coordinate *move);
+
+
+// node structure for tree
+typedef struct Node {
+  Board board;
+  struct Node *children; // array of either pieces that can jump (preferred), or just move
+  int depth;
+  int alpha;
+  int beta;
+  int value; // # pieces: COMPUTER - USER;
+} Node;
+
+Node *init_node(); // initialize tree node
+void free_tree(Node *t); // free mallocs
+void print_tree(Node *t); // prints tree using preorder traversal (for checking)
---
+++ node.c
@@ -0,0 +1,31 @@
+#include "log.h"
+
+// initialize tree node with children
+Node *init_node() {
+  Node *n = malloc(sizeof(Node));
+  check_null(n, "Unable to allocate node");
+
+  return n;
+}
+
+// free mallocs for all nodes in the tree using post-order traversal
+void free_tree(Node *t) {
+    if (t->left != NULL)
+      free_tree(t->left);
+    if (t->right != NULL)
+      free_tree(t->right);
+    if (t != NULL) {
+      free(t);
+    }
+}
+
+// prints tree values using preorder traversal (for checking)
+void print_tree(Node *t) {
+  if (isLeaf(t))
+    printf("%d", (t->COMPUTER)-(t->USER));
+  else {
+    printf("-"); // to check tree for testing
+    print_tree(t->left);
+    print_tree(t->right);
+  }
+}
