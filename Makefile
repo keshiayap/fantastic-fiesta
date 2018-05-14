5329e81007af063489587b8598f402051a96408a
KaiTingKeshia Yap <kyap@clyde>
2018-05-13 23:42:28 -0400

    rearranged things in log.h file

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all : main
 
-log.o: board.mm checkmove.mm log.h
-	g++ -c board.mm checkmove.mm log.h
+log.o: board.mm checkmove.mm node.mm log.h
+	g++ -c board.mm checkmove.mm node.mm log.h
 
 ai.o: ai.mm log.h
 	g++ -c ai.mm log.h
@@ -10,4 +10,4 @@ main: ai.o log.o
 	g++ -std=c++0x -Wall -o main main ai.o log.o
 
 clean:
-	rm *.o main
+	rm log.o ai.o main
--- log.h
+++ log.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <memory>
-#include <QDebug>
+//#include <QDebug>
 #include <ctype.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -16,46 +16,25 @@ using namespace std;
 #define MIN = numeric_limits<int>::min();
 
 
-class Board {
- public:
-  int COMPUTER; int USER; // # of pieces at start of a game
-  /* coordinate compList[12]; */
-  /* coordinate userList[12]; */
-
-  char board[8][8];
-  void updateBoard(coordinate current, coordinate next, char turn, bool jump);
-  void flip(); // reverse direction (change turn)
-  Board(); // construct new board
-  int count(char turn); // count number of C(omputer) or U(ser) pieces left on\
-   the board
-     void toString(); // print to stdout
-
-};
-
-// Board functions
-Board::Board();
-void Board::updateBoard(coordinate curr, coordinate next, char player, bool jump);
-void Board::flip();
-void Board::toString();
-Board Board::copyBoard(Board board);
-
 // easy tuple to use to find a position on the board
 typedef struct coordinate {
   int x;
   int y;
 } coordinate;
 
-// move functions
-void check_null(void *ptr, char *msg);
-coordinate init(int a, int b);
-coordinate *buildMove(int r1, int c1, int r2, int c2);
-bool canMove(int player, int r1, int c1, int r2, int c2);
-bool canJump(int player, int r1, int c1, int r2, int c2);
-coordinate **getMoves(int player);
-coordinate **getJumps(int player);
-coordinate getPiece(coordinate *move);
-coordinate getDest(coordinate *move);
 
+class Board {
+ public:
+  int COMPUTER, USER; // # of pieces at start of a game
+  char board[8][8];
+
+  Board(); // construct new board
+  void updateBoard(coordinate current, coordinate next, char turn, bool jump);
+  void flip(); // reverse direction (change turn)
+  int count(char turn); // count number of C(omputer) or U(ser) pieces left on the board
+  void toString(); // print to stdout
+  Board copyBoard(Board board);
+};
 
 // node structure for tree
 typedef struct Node {
@@ -68,6 +47,20 @@ typedef struct Node {
   int value; // # pieces: COMPUTER - USER;
 } Node;
 
+// move functions
+void check_null(void *ptr, char *msg);
+coordinate init(int a, int b);
+coordinate *buildMove(int r1, int c1, int r2, int c2);
+bool canMove(int player, int r1, int c1, int r2, int c2);
+bool canJump(int player, int r1, int c1, int r2, int c2);
+coordinate **getMoves(int player);
+coordinate **getJumps(int player);
+coordinate getPiece(coordinate *move);
+coordinate getDest(coordinate *move);
+
+// node functions
 Node *init_node(); // initialize tree node
 void free_tree(Node *t); // free mallocs
 void print_tree(Node *t); // prints tree using preorder traversal (for checking)
+
+#endif
