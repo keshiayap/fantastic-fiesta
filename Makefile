03fb7d04899a55890a78dede29cfda68ad0b3aa6
KaiTingKeshia Yap <kyap@clyde>
2018-05-14 19:35:12 -0400

    changed Board from class to struct

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
-all : main
+all : main board.o
+
+board.o: board.cpp
+	g++ -c board.cpp
+
+checkmove.o: checkmove.cpp
+	g++ -c checkmove.cpp
+
+node.o: node.cpp
+	g++ -c node.cpp
+
+ai.o: ai.cpp
+	g++ -c ai.cpp
+
 
 log.o: board.cpp checkmove.cpp node.cpp ai.cpp
 	g++ -c board.cpp checkmove.cpp node.cpp ai.cpp
--- log.h
+++ log.h
@@ -9,48 +9,42 @@
 #include <limits.h>
 #include <string.h>
 #include <random>
+#include <limits.h>
 
 using namespace std;
 #define C 1
 #define U 0
-#define MAX = numeric_limits<int>::max();
-#define MIN = numeric_limits<int>::min();
 
-
-// easy tuple to use to find a position on the board
 typedef struct coordinate {
-  int x;
-  int y;
+  int row;
+  int col;
 } coordinate;
 
-
-class Board {
- public:
-  int COMPUTER, USER; // # of pieces at start of a game
-  char board[8][8];
-
-  Board(); // construct new board
-  void updateBoard(coordinate current, coordinate next, char turn, bool jump);
-  void flip(); // reverse direction (change turn)
-  int count(char turn); // count number of C(omputer) or U(ser) pieces left on the board
-  void toString(); // print to stdout
-  Board copyBoard(Board board);
-};
+typedef struct Board {
+  static int COMPUTER, USER; // # of pieces at start of a game
+  static char board[8][8];
+} Board;
+  
+// board functions
+Board init_board(int COMPUTER = 12*3, int USER = 12*3); // construct new board
+void updateBoard(Board board, coordinate current, coordinate next, char turn, bool jump);
+void flip(Board board); // reverse direction (change turn)
+void toString(Board board); // print to stdout
+Board copyBoard(Board board); // duplicate
 
 // node structure for tree
 typedef struct Node {
   Board board;
-  struct Node *children; // array of either pieces that can jump (preferred), or just move
+  struct Node **children; // array of either pieces that can jump (preferred), or just move
+  coordinate *bestMove;
   int numChildren;
-  int depth;
   int alpha;
   int beta;
   int value; // # pieces: COMPUTER - USER;
 } Node;
 
 // move functions
-void check_null(void *ptr, char *msg);
-bool isEmpty(void *ptr);
+void check_null(void *ptr, char const *msg);
 coordinate init(int a, int b);
 coordinate *buildMove(int r1, int c1, int r2, int c2);
 bool canMove(int player, int r1, int c1, int r2, int c2);
@@ -59,14 +53,15 @@ coordinate **getMoves(int player);
 coordinate **getJumps(int player);
 
 // node functions
-Node *init_node(); // initialize tree node
+Node init_node(); // initialize tree node
 void free_tree(Node *t); // free mallocs
 void print_tree(Node *t); // prints tree using preorder traversal (for checking)
 
 // intelligence functions
+int size(coordinate **list);
+bool isEmpty(coordinate **list);
 coordinate *makeRandomMove(Board board, char player);
 Node *buildTree(Board board, coordinate *move, int depth, char player, bool jump);
 int minimax(Node *t, int depth, bool isMaximizingPlayer, int alpha, int beta, coordinate *bestMove);
-
 
 #endif
