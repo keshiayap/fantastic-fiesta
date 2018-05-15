00bd3baa056e632385f8260e1d2c0337848c794d
KaiTingKeshia Yap <kyap@clyde>
2018-05-14 21:23:55 -0400

    debugged compiler errors

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 all : main board.o
 
 board.o: board.cpp
-	g++ -c board.cpp
+	g++ -std=c++0x -Wall -c board.cpp
 
 checkmove.o: checkmove.cpp
-	g++ -c checkmove.cpp
+	g++ -std=c++0x -Wall -c checkmove.cpp
 
 node.o: node.cpp
-	g++ -c node.cpp
+	g++ -std=c++0x -Wall -c node.cpp
 
 ai.o: ai.cpp
-	g++ -c ai.cpp
+	g++ -std=c++0x -Wall -c ai.cpp
 
 
 log.o: board.cpp checkmove.cpp node.cpp ai.cpp
-	g++ -c board.cpp checkmove.cpp node.cpp ai.cpp
+	g++ -std=c++0x -Wall -c board.cpp checkmove.cpp node.cpp ai.cpp
 
 main: log.o main.cpp
 	g++ -std=c++0x -Wall -o main main.cpp log.o
--- log.h
+++ log.h
@@ -1,9 +1,11 @@
+// contains all declarations
+
 #ifndef LOG_H
 #define LOG_H
 
+//#include <QDebug>
 #include <iostream>
 #include <memory>
-//#include <QDebug>
 #include <ctype.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -12,52 +14,66 @@
 #include <limits.h>
 
 using namespace std;
-#define C 1
-#define U 0
+#define C 1 // computer regular piece
+#define CK 2 // computer king piece
+#define U -1 // user regular piece
+#define UK -2 // user king piece
 
+/* ------------------------------------------ */
+/* -----------------STRUCTS------------------ */
+/* ------------------------------------------ */
+
+// tuple for finding position on board
 typedef struct coordinate {
   int row;
   int col;
 } coordinate;
 
+// Board array and total player values
 typedef struct Board {
   static int COMPUTER, USER; // # of pieces at start of a game
   static char board[8][8];
 } Board;
   
-// board functions
-Board init_board(int COMPUTER = 12*3, int USER = 12*3); // construct new board
-void updateBoard(Board board, coordinate current, coordinate next, char turn, bool jump);
-void flip(Board board); // reverse direction (change turn)
-void toString(Board board); // print to stdout
-Board copyBoard(Board board); // duplicate
-
-// node structure for tree
+// node structure for minimax tree
 typedef struct Node {
   Board board;
   struct Node **children; // array of either pieces that can jump (preferred), or just move
+  coordinate **list;
   coordinate *bestMove;
   int numChildren;
   int alpha;
   int beta;
-  int value; // # pieces: COMPUTER - USER;
+  int value; // diff in # pieces: COMPUTER - USER;
 } Node;
 
-// move functions
+/* ------------------------------------------ */
+/* ----------------FUNCTIONS----------------- */
+/* ------------------------------------------ */
+
+// board.cpp functions
 void check_null(void *ptr, char const *msg);
+Board *init_board(int COMPUTER = 12*3, int USER = 12*3); // construct new board, default parameters
+Board *copyBoard(Board *board); // duplicate
+void updateBoard(Board *board, coordinate curr, coordinate next, char player, bool jump);
+/* void flip(Board *board); // reverse direction (change turn) */
+void toString(Board *board); // print to stdout
+
+// checkmove.cpp move functions
+bool isEmpty(void **ptr);
 coordinate init(int a, int b);
 coordinate *buildMove(int r1, int c1, int r2, int c2);
-bool canMove(int player, int r1, int c1, int r2, int c2);
-bool canJump(int player, int r1, int c1, int r2, int c2);
-coordinate **getMoves(int player);
-coordinate **getJumps(int player);
+bool canMove(Board *board, int player, int r1, int c1, int r2, int c2);
+bool canJump(Board *board, int player, int r1, int c1, int r2, int c2);
+coordinate **getMoves(Board *board, int player);
+coordinate **getJumps(Board *board, int player);
 
-// node functions
-Node init_node(); // initialize tree node
+// node.cpp functions
+Node *init_node(Board *board); // initialize tree node
 void free_tree(Node *t); // free mallocs
-void print_tree(Node *t); // prints tree using preorder traversal (for checking)
+void print_tree(Node *t, char *tab, int depth); // prints tree using preorder traversal (for checking)
 
-// intelligence functions
+// ai.cpp intelligence functions
 int size(coordinate **list);
 bool isEmpty(coordinate **list);
 coordinate *makeRandomMove(Board board, char player);
